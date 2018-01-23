f6a78512bd7f553acd370827320ea3fc65eceda4
sanoousmane <sanoousmane@gmail.com>
2018-01-23 11:09:58 +0000

    Delete old file oracle-rdbms-server-12cR1-preinstall.conf

--- files/setEnv.sh
+++ files/setEnv.sh
@@ -1,15 +1,15 @@
 # Oracle Settings
 export TMP=/tmp
-export TMPDIR=\$TMP
+export TMPDIR=$TMP
 
 export ORACLE_HOSTNAME=db.example.com
 export ORACLE_UNQNAME=cdb1
 export ORACLE_BASE=/u01/app/oracle
-export ORACLE_HOME=\$ORACLE_BASE/product/12.2.0.1/db_1
+export ORACLE_HOME=$ORACLE_BASE/product/12.2.0.1/db_1
 export ORACLE_SID=cdb1
 
-export PATH=/usr/sbin:/usr/local/bin:\$PATH
-export PATH=\$ORACLE_HOME/bin:\$PATH
+export PATH=/usr/sbin:/usr/local/bin:$PATH
+export PATH=$ORACLE_HOME/bin:$PATH
 
-export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
-export CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
+export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
